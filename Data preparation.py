b7e6189f57d6b5847491cd8d54eb5e25a53caa7d
gilangw1024 <gilangwjnrk@gmail.com>
2025-09-24 08:22:00 +0700

    Update Data preparation.py

--- Data preparation.py
+++ Data preparation.py
@@ -14,14 +14,3 @@ train_datagen = ImageDataGenerator(
     height_shift_range=0.1,
     fill_mode='nearest'
 )
-
-#path to drive for training set
-training_set = train_datagen.flow_from_directory(
-    '/content/drive/MyDrive/Dataset/bird classification/train',
-    target_size=(224, 224),
-    batch_size=32,
-    class_mode='categorical'
-)
-
-print("Count Class:", training_set.num_classes)
-print("Class Label:", training_set.class_indices)
